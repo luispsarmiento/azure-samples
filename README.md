f25d554690f6d3b673c3b5b15d0b91de805a5fde
Luis Puc <j.louis1493@gmail.com>
2024-07-29 00:57:14 +0000

    Changed: implementing send email (dont tested)

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 A collection of templates that implement various Azure cloud services.
 
 ## Send Email function
+### Compile to run localy
+```bash
+cargo build --release
+cp target/release/send-email ./send-email-result
+func start
+```
+### Compile to deploy
 Compile the send-email function to Linux/x64. A binary named send-email is created. Copy it to the function app root.
 ```bash
 rustup target add x86_64-unknown-linux-musl
 cargo build --release --target=x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/handler .
+cp target/x86_64-unknown-linux-musl/release/send-email ./send-email-af
 ```
--- compute/af/src/main.rs
+++ compute/af/src/main.rs
@@ -1,10 +1,22 @@
+extern crate uuid;
+extern crate lettre;
+extern crate native_tls;
+
 use std::collections::HashMap;
 use std::env;
 use std::net::Ipv4Addr;
 use warp::{http::Response, Filter};
 
+use lettre::{SendableEmail, EmailAddress, EmailTransport};
+use lettre::smtp::{SmtpTransportBuilder, SUBMISSION_PORT};
+use lettre::smtp::authentication::Credentials;
+use lettre::smtp::client::net::ClientTlsParameters;
+
+use native_tls::TlsConnector;
+
 #[tokio::main]
 async fn main() {
+    sendEmail("Web Form", "j.louis1493@gmail.com"); 
     let example1 = warp::get()
         .and(warp::path("api"))
         .and(warp::path("send-email"))
@@ -21,4 +33,55 @@ async fn main() {
     };
 
     warp::serve(example1).run((Ipv4Addr::LOCALHOST, port)).await
+}
+
+// SEND EMAIL SECTION
+struct CrashReport {
+    to: Vec<EmailAddress>,
+    from: EmailAddress,
+    message_id: String,
+    message: Vec<u8>,
+}
+
+// A simple constructor for our email.
+impl CrashReport {
+    pub fn new(from_address: EmailAddress,
+        to_addresses: Vec<EmailAddress>,
+        message_id: String,
+        message: String) -> CrashReport {
+            CrashReport { from: from_address,
+            to: to_addresses,
+            message_id: message_id,
+            message: message.into_bytes()
+            }
+        }
+}
+
+impl<'a> SendableEmail<'a, &'a [u8]> for CrashReport {
+    fn to(&self) -> Vec<EmailAddress> {
+        self.to.clone()
+    }
+
+    fn from(&self) -> EmailAddress {
+        self.from.clone()
+    }
+
+    fn message_id(&self) -> String {
+        self.message_id.clone()
+    }
+
+    fn message(&'a self) -> Box<&[u8]> {
+        Box::new(self.message.as_slice())
+    }
+}
+
+fn sendEmail(emisor: String, email: String){
+    let server = "smtp.ionos.mx";
+    let connector = TlsConnector::builder().unwrap().build().unwrap();
+    let mut transport = SmtpTransportBuilder::new((server, SUBMISSION_PORT), lettre::ClientSecurity::Opportunistic(<ClientTlsParameters>::new(server.to_string(), connector)))
+                        .expect("Failed to create transport")
+                        .credentials(Credentials::new(env::var("USERNAME").unwrap_or_else(|_| "contacto@luispsarmiento.com".to_string()), env::var("PASSWORD").unwrap_or_else(|_| "pass".to_string())))
+                        .build();
+    let report = CrashReport::new(EmailAddress::new(email.to_string()), vec![EmailAddress::new("contacto@luispsarmiento.com".to_string())], "foo".to_string(), "OOPS!".to_string());
+    transport.send(&report).expect("Failed to send the report");
 }
